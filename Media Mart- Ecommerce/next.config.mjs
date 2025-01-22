ea5f06a41e5f2b394f536a53946b255c8fa1ae5f
Nida Muhammad Nasar <nidashafqet@gmail.com>
2025-01-22 08:54:15 +0500

    Delete Media Mart- Ecommerce directory

--- Media Mart- Ecommerce/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['cdn.sanity.io'], // Add 'cdn.sanity.io' here
-  },
-};
-  
-  export default nextConfig;
-  
